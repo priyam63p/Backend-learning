16c22277cb9dde80722d70aed76c633941acdf07
priyam63p <priyampatra63@gmail.com>
2026-09-17 20:21:06 +0530

    Lec_11 Added

---
+++ Lec11/middleware/auth.js
@@ -0,0 +1,13 @@
+const Auth=(req, res, next) => {
+  const token = "ABCDEF";
+  const Access = token === "ABCDEF" ? 1 : 0;
+
+  if (!Access) {
+    res.status(403).send("No Permission");
+  }
+  next();
+};
+
+module.exports={
+    Auth
+}
